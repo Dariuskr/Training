b04da5c8cfd3991b19fe6d31eab58a8750e9035d
teinted321 <darius.krei@gmail.com>
2017-04-01 18:46:58 +0300

    Improved guessing game + fixing aligment for a clearner look

--- script.js
+++ script.js
@@ -3,18 +3,18 @@ var guess;
 var guessCount = 0;
 var correctGuess = false;
 
-function getRandomNumber( upper ) {
-  var num = Math.floor(Math.random() * upper) + 1; 
-  return num;
+function getRandomNumber(upper) {
+    var num = Math.floor(Math.random() * upper) + 1;
+    return num;
 }
 
 do {
     guess = prompt('I am thinking of a number between 1 and 10 ')
     guess += 1;
-  if (parseInt(guess) === randomNumber){
+    if (parseInt(guess) === randomNumber) {
 
-  }
-} while ( ! correctGuess )
+    }
+} while (!correctGuess)
 
-    document.write('<h1> You guessed the number! </h1>');
-    document.write('It took you ' + guessCount +' tries to guess the number' + randomNumber)
+document.write('<h1> You guessed the number! </h1>');
+document.write('It took you ' + guessCount + ' tries to guess the number' + randomNumber)
