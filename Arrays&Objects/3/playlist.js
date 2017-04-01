df314cbc19798eda446ebb44527eecee95dc0a86
teinted321 <darius.krei@gmail.com>
2017-04-01 21:01:07 +0300

    push , unshift

---
+++ Arrays&Objects/3/playlist.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Max on 2017.04.01.
+ */
+var playlist = [];
+playlist.push('Shape of you' , 'idfc' , 'Dirty Laundry' , 'Cars,Clothes,Calories');
+playlist.unshift('Give a little more' , 'One more night');
+printList(playlist);
